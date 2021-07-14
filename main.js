4ab02d64402ff91436beefad5873be8db54d74c6
pjwhite08 <pamelajeanwhite@gmail.com>
2021-07-13 20:19:56 -0500

    typos

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ const rl = readline.createInterface({
 //The variable stacks represents the board. 
 //The board is defined as an object with three key: value pairs. 
 //Each value is an array. 
-//The numbers in the array represent the size of the stones (or pieces that will be moved according to the rules of the game.
+//The numbers in the array represent the size of the stones (or pieces that will be moved according to the rules of the game).
 let stacks = {
   a: [4, 3, 2, 1],
   b: [],
@@ -36,12 +36,12 @@ const printStacks = () => {
 //and define variables (indexOfLastStartStack and indescOfLastEndStack) that represent last index of the last/ top stone of each stack
 //Once we can access the index of the the last element in each array, we can 
 //define variables that represent the size of the top stone of each stack, lastStart and lastEnd 
-//in order to compare those values to follow the rules of the game
-// The conditional statement conmpares the last stone (element) in each stack (array)
+//in order to compare those values to follow the rules of the game.
+// The conditional statement compares the last stone (element) in each stack (array)
 // If the startStack has a length of zero, (i.e., the stack is empty) the function returns false. You can't grab a stone that isn't there.
 //If the endStack has a length of zero, (i.e., is empty) you can always place a stone there. Stones can always go on empty stacks.
 //Or if the stone from the top/end of the startStack (lastStart) is smaller than the stone on the top/end of the endStack (lastEnd)
-//so you can make those moves and the function returns true
+//you can make those moves and the function returns true
 //Or if the stone from the top/end of the startStack is larger than the stone on the top/end of the endStack,
 //that move is illegal and the function returns false
 const isLegal = (startStack, endStack) => {
@@ -85,7 +85,7 @@ const movePiece = (startStack, endStack) => {
 //checkForWin() checks for the two possible winning conditions
 // if the value at each index of stack b is equal to 4, 3, 2, and 1 (respetively) or
 // if the same is true for stack c
-// the function returns true with and prints a winning message
+// the function returns true and prints a winning message
 //otherwise, the function returns false and the game keeps playing
 //I realize this could be done with a much shorter line of code, but conceptually, this made the most sense to me, so I did it. 
 const checkForWin = () => {
@@ -103,15 +103,15 @@ else {
 //The first conditional statement checks to see if
 //you entered the same string twice
 //in which case it returns "You can't start and end on the same stack"
-//and returns false, which makes no changes and prompts you to enter new stacks
-//I realize that, at this point, you could have entered two of the same anything - not necessarily letters that represented a stack - and this would give a return message as if you had entered a stack letter
-//this conditional statement then checks to make sure that you have entered only a, b, or c in each stack
-// and if you did, it prints the letter of the startStack tower  and endStack you chose and allows the rest of the function to run
+//and returns false, which makes no changes, exits the function, and prompts you to enter new stacks
+//I realize that, at this point, you could have entered two of the same anything - not necessarily letters that represented a stack - and this would give a return message as if you had entered a stack letter.
+//This conditional statement then checks to make sure that you have entered only a, b, or c in each stack
+// and if you did, it prints the letter of the startStack tower and endStack you chose and allows the rest of the function to run
 //Otherwise, if anything else was input, the function prints "Please choose only a, b, or c" 
 //and returns false, prompting you to enter start and end stack letters again
 
-//After we know appropriate parameters have been entered, the results of isLegal() are saved as a variable, legalMove
-//so we can use this information to decide to continue or stop and return to choosing start and end stacks
+//After we know appropriate parameters have been entered, the results of isLegal() are saved as a variable, legalMove,
+//so we can use this information to decide to continue or stop and return to choosing start and end stacks.
 //If legalMove is false, you are prompted to re-enter a start stack and end stack
 //If legalMove is true, the game continues to movePiece()
 
@@ -138,7 +138,7 @@ const towersOfHanoi = (startStack, endStack) => {
   }
 
   let legalMove = isLegal(startStack, endStack)
-    if (legalMove == false) {
+    if (!legalMove) {
     return false}
     
   startStack = stacks[startStack]
